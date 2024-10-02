20118c36f24446721e04f56120df290102ac901d
FainNovity <160344269+FainNovity@users.noreply.github.com>
2024-10-02 23:23:24 +0530

    Add files via upload

--- src/components/mainUI/ChatGround.js
+++ src/components/mainUI/ChatGround.js
@@ -8,23 +8,23 @@ function ChatGround(props){
         
         let formattedData;
          formattedData = data.map(data=><ChatBubble user={user} sender={data[0]} data={data[1]} />);
-
+         
          socket.on('update',({user,message})=>{
-             console.log(message);
+             
              setData([...data,[user,message]]);
-         });
-         socket.on('joined',(message)=>{
-             setData([...data,["",message]]);
-         });
-         socket.on('exiting',(message)=>{
-             setData([...data,["",message]]);
-         });
-
-        useEffect(()=>{
+            });
+            socket.on('joined',(message)=>{
+                setData([...data,["",message]]);
+            });
+            socket.on('exiting',(message)=>{
+                setData([...data,["",message]]);
+            });
+            
+            useEffect(()=>{
             console.log("setting up")
         },[socket]);
         useEffect(()=>{
-            fetch(process.env.DB+port).then(res=>res.json()).then(res=>setData(res));
+            fetch(process.env.REACT_APP_DB+'/'+port).then(res=>res.json()).then(res=>setData(res));
         },[]);
 
         scrollRef.current?.scrollIntoView({behavior: 'smooth'});
